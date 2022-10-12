635e96c08cd13f0568a30cddc6d5a1a80f3423db
RikkeHein <rikke-hein@hotmail.com>
2022-10-12 10:11:30 +0200

    please virk

--- Model/Comment.cs
+++ Model/Comment.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 namespace miniprojektreddit.Model
 {
-	public class Comment
-	{
-		public Comment()
-		{
-		}
-	}
+    public class Comment
+    {
+        public Comment(string text, User author, DateTime date)
+        {
+            this.Text = text;
+            this.Author = author;
+            this.Date = date;
+        }
+
+        public Comment()
+        {
+
+        }
+
+        public long CommentId { get; set; }
+        public string Text { get; set; }
+        public User? Author { get; set; }
+        public DateTime Date { get; set; }
+        public bool Upvote { get; set; }
+        public bool Downvote { get; set; }
+        public int UpvoteCount { get; set; }
+        public int DownvoteCount { get; set; }
+
+    }
 }
 
--- Model/Thread.cs
+++ Model/Thread.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 namespace miniprojektreddit.Model
 {
-	public class Thread
-	{
-		public Thread()
-		{
-		}
-	}
+    public class Thread
+    {
+        public Thread(string title, User author, string text, DateTime date)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Text = text;
+            this.Date = date;
+        }
+
+        public Thread()
+        {
+
+        }
+
+        public long ThreadId { get; set; }
+        public string Title { get; set; }
+        public User? Author { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public bool Upvote { get; set; }
+        public bool Downvote { get; set; }
+        public int UpvoteCount { get; set; }
+        public int DownvoteCount { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
 }
 
